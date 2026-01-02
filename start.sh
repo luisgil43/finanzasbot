25e9e6410f3e1c473d37ce7586bf97deb5b749e7
Luis Enrique Gil Moya <luisenriquegilmoya@MacBook-Pro-de-Luis.local>
2026-01-02 11:34:32 -0300

    Actualizacion

--- start.sh
+++ start.sh
@@ -5,7 +5,8 @@ echo "Running migrations..."
 python manage.py migrate --noinput
 
 echo "Starting gunicorn..."
+# Render inyecta $PORT automáticamente
 exec gunicorn config.wsgi:application \
   --bind 0.0.0.0:${PORT:-8000} \
-  --workers ${WEB_CONCURRENCY:-1} \
+  --workers ${WEB_CONCURRENCY:-2} \
   --timeout 120
