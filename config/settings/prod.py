7d96d507d029d769840895779d038b799f63dd57
Luis Enrique Gil Moya <luisenriquegilmoya@MacBook-Pro-de-Luis.local>
2026-01-02 20:04:32 -0300

    Agrego actualizacion 10

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,4 +1,6 @@
 # config/settings/prod.py
+import dj_database_url
+
 from .base import *
 
 DEBUG = False
@@ -12,3 +14,18 @@ SECURE_HSTS_INCLUDE_SUBDOMAINS = env_bool("SECURE_HSTS_INCLUDE_SUBDOMAINS", True
 SECURE_HSTS_PRELOAD = env_bool("SECURE_HSTS_PRELOAD", True)
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+# -----------------------------
+# ✅ DB en Render (PostgreSQL)
+# -----------------------------
+DATABASE_URL = (env("DATABASE_URL", "") or "").strip()
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL no está configurada en Render (Web Service).")
+
+DATABASES = {
+    "default": dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=600,
+        ssl_require=False,  # si tu URL trae sslmode=require, dj_database_url lo respeta igual
+    )
+}
